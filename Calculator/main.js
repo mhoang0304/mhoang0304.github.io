6bd2f9840d84e8c207ce11950d2afe54c02968bd
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2020-12-11 17:45:19 +0700

    add calculator

---
+++ Calculator/main.js
@@ -0,0 +1,60 @@
+//Set Time:
+let show_hour = document.querySelector(".hour");
+let show_minutes = document.querySelector(".minutes");
+
+function updateTime() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+
+    show_hour.innerText = hour;
+    show_minutes.innerText = minutes;
+}
+
+setInterval(updateTime, 1000);
+
+//Calculator:
+let input = document.querySelector(".input");
+let items = Array.from(document.querySelectorAll(".item"));
+let string = "";
+
+items.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        if (input.innerHTML == "0") {
+            input.innerHTML = "";
+        }
+        if (btn.innerHTML == "AC") {
+            input.innerHTML = "0";
+            string = "";
+        } else if (btn.innerHTML == "DEL") {
+            let arrText = Array.from(input.innerHTML);
+            arrText.splice(arrText.length - 1, 1);
+            if (arrText.length != 0) {
+                input.innerHTML = arrText.join("");
+            } else {
+                input.innerHTML = "0";
+            }
+        } else if (btn.value == "=") {
+            input.innerHTML = eval(string);
+        } else {
+            input.innerHTML += btn.innerHTML;
+            string += btn.value;
+            console.log(string);
+        }
+    })
+})
+
+//Change Theme:
+let background = document.body.style.background;
+
+function theme() {
+    if(document.body.style.background == "white") {
+        document.body.style.background = "linear-gradient(45deg, #CCFFFF, #FFCCCC)"
+    } else {
+        document.body.style.background = "white";
+    }
+}
+
+
+
+
