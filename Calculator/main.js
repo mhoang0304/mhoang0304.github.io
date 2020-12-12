c203dfbcf54973e336dbc02c5f18f21c101ac324
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2020-12-12 16:48:21 +0700

    update

--- Calculator/main.js
+++ Calculator/main.js
@@ -7,53 +7,44 @@ function updateTime() {
     let hour = now.getHours();
     let minutes = now.getMinutes();
 
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+
     show_hour.innerText = hour;
     show_minutes.innerText = minutes;
 }
-
+updateTime();
 setInterval(updateTime, 1000);
 
-//Calculator:
-let input = document.querySelector(".input");
-let items = Array.from(document.querySelectorAll(".item"));
-let string = "";
-
-items.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-        if (input.innerHTML == "0") {
-            input.innerHTML = "";
-        }
-        if (btn.innerHTML == "AC") {
-            input.innerHTML = "0";
-            string = "";
-        } else if (btn.innerHTML == "DEL") {
-            let arrText = Array.from(input.innerHTML);
-            arrText.splice(arrText.length - 1, 1);
-            if (arrText.length != 0) {
-                input.innerHTML = arrText.join("");
-            } else {
-                input.innerHTML = "0";
-            }
-        } else if (btn.value == "=") {
-            input.innerHTML = eval(string).toFixed(2).replace(/\.?0*$/g,'');
-        } else {
-            input.innerHTML += btn.innerHTML;
-            string += btn.value;
-            console.log(string);
-        }
-    })
-})
-
 //Change Theme:
-
 function theme() {
-    if(document.body.style.background == "white") {
+    if (document.body.style.background == "white") {
         document.body.style.background = "linear-gradient(45deg, #CCFFFF, #FFCCCC)";
     } else {
         document.body.style.background = "white";
     }
 }
 
+//Calculator:
+let screenDisplay = document.querySelector(".input");
 
+function input(value) {
+    if (screenDisplay.innerText == 0) {
+        screenDisplay.innerText = "";
+    }
+    screenDisplay.innerText += value;
+    console.log(screenDisplay.innerText);
+}
+
+function reset() {
+    screenDisplay.innerText = 0;
+}
+
+function equal() {
+    let result = screenDisplay.innerText.replaceAll("×", "*").replaceAll("–", "-").replaceAll("÷", "/");
+    screenDisplay.innerText = eval(result).toFixed(2).replace(/\.?0*$/g,'');
+    console.log(result);
+}
 
 
