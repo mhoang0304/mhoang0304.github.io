b262108514bb72d2143a7c25424cffe38094f5af
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2020-12-13 00:14:53 +0700

    done code calculator

--- Calculator/main.js
+++ Calculator/main.js
@@ -8,7 +8,11 @@ function updateTime() {
     let minutes = now.getMinutes();
 
     if (minutes < 10) {
-        minutes = "0" + minutes
+        minutes = "0" + minutes;
+    }
+
+    if(hour < 10) {
+        hour = "0" + hour;
     }
 
     show_hour.innerText = hour;
@@ -16,6 +20,75 @@ function updateTime() {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+//Calculator:
+let screenDisplay = document.querySelector(".input");
+
+function input(value) {
+    //Xoá số 0 khi nhập vào
+    if (screenDisplay.innerHTML === "0") {
+        screenDisplay.innerHTML = "";
+        //Kiểm tra trường hợp vô nghĩa ban đầu
+        if (value == "%" || value == "<sup>2</sup>" || value == ")") {
+            value = "0";
+        }
+        if (value == "." || value == "+" || value == "-" || value == "×" || value == "÷") {
+            value = "0" + value;
+        }
+    }
+
+    //Kiểm tra trường hợp lặp phép toán
+    let check = screenDisplay.innerHTML.slice(-1);
+    console.log("kiểm tra giá trị cuối là: " + check);
+    if (check == "+" || check == "-" || check == "×" || check == "÷") {
+        if (value == "%" || value == "<sup>2</sup>" || value == "÷" || value == "×" || value == "+" || value == ".") {
+            value = "";
+        }
+    }
+    if (check == ".") {
+        if (value == "%" || value == "<sup>2</sup>" || value == "√(" || value == "(" || value == ")" || value == "÷" || value == "×" || value == "+" || value == "." || value == "-") {
+            value = "";
+        }
+    }
+    if (check == "%") {
+        if (value == "%" || value == "√(" || value == "(" || value == ")" || value == "÷" || value == "×" || value == "+" || value == "." || value == "-") {
+            value = "";
+        }
+    }
+    if (check == ">" || check == ")") { //Bình phương
+        if (value == "%" || value == "<sup>2</sup>" || value == "√(" || value == "(" || value == ")" || value == ".") {
+            value = "";
+        }
+    }
+    if (check == "(") { 
+        if (value == "%" || value == "<sup>2</sup>" || value == "(" || value == "×" || value == "÷"  || value == ".") {
+            value = "";
+        }
+    }
+
+    screenDisplay.innerHTML += value;
+
+    if (screenDisplay.innerHTML !== "0") {
+        document.querySelector(".reset").innerText = "C";
+    }
+}
+
+function del() {
+    screenDisplay.innerHTML = screenDisplay.innerHTML.slice(0, -1);
+    if (screenDisplay.innerHTML.length == 0) {
+        screenDisplay.innerHTML = "0";
+    }
+}
+
+function reset() {
+    screenDisplay.innerHTML = "0";
+    document.querySelector(".reset").innerText = "AC";
+}
+
+function equal() {
+    let result = screenDisplay.innerHTML.replaceAll("×", "*").replaceAll("÷", "/").replaceAll("√(", "Math.sqrt(").replaceAll("%", "*0.01").replaceAll("<sup>2</sup>", "**2");
+    screenDisplay.innerHTML = eval(result).toFixed(2).replace(/\.?0*$/g, '');
+}
 
 //Change Theme:
 function theme() {
@@ -25,26 +98,3 @@ function theme() {
         document.body.style.background = "white";
     }
 }
-
-//Calculator:
-let screenDisplay = document.querySelector(".input");
-
-function input(value) {
-    if (screenDisplay.innerText == 0) {
-        screenDisplay.innerText = "";
-    }
-    screenDisplay.innerText += value;
-    console.log(screenDisplay.innerText);
-}
-
-function reset() {
-    screenDisplay.innerText = 0;
-}
-
-function equal() {
-    let result = screenDisplay.innerText.replaceAll("×", "*").replaceAll("–", "-").replaceAll("÷", "/");
-    screenDisplay.innerText = eval(result).toFixed(2).replace(/\.?0*$/g,'');
-    console.log(result);
-}
-
-
