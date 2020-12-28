5d6198472c1d30b4122d36c85a726a634f428fe9
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2020-12-28 12:04:10 +0700

    update

--- turtle/js/main.js
+++ turtle/js/main.js
@@ -4,52 +4,51 @@ $(document).ready(function () {
         let imageNext = $(".active").next();
 
         // Xử lí trạng thái ảnh
-        // let status = $(".light").index() + 1;
+        let status = $(".light").index() + 1;
 
-        // $(".status__items").removeClass("light");
+        $(".status__items").removeClass("light");
 
-        // if (status == $(".status__items").length) {
-        //     status = 0;
-        // }
-        // $(".status__items:nth-child(" + (status + 1) + ")").addClass("light");
+        if (status == $(".status__items").length) {
+            status = 0;
+        }
+        $(".status__items:nth-child(" + (status + 1) + ")").addClass("light");
 
-        //     if (imageNext.length == 0) { // Đến slide cuối cùng
-        //         $(".active").addClass("move-out").one("webkitAnimationEnd", function () {
-        //             $(".move-out").removeClass("move-out");
-        //         });
-
-        //         $(".carousel__img:first-child").addClass("move-in").one("webkitAnimationEnd", function () {
-        //             $(".active").removeClass("active");
-        //             $(".move-in").addClass("active").removeClass("move-in");
-        //         });
-        //     } else {
-        //         $(".active").addClass("move-out").one("webkitAnimationEnd", function () {
-        //             $(".move-out").removeClass("move-out");
-        //         });
-
-        //         imageNext.addClass("move-in").one("webkitAnimationEnd", function () {
-        //             $(".active").removeClass("active");
-        //             $(".move-in").addClass("active").removeClass("move-in");
-        //         });
-        //     }
-        // }, 3000);
-
-        // Xử lí khi click vào nút
-        $(".status__items").click(function () {
-            clearInterval(carousel);
-
-            $(".status__items").removeClass("light");
-
-            $(this).addClass("light");
-
+        if (imageNext.length == 0) { // Đến slide cuối cùng
             $(".active").addClass("move-out").one("webkitAnimationEnd", function () {
                 $(".move-out").removeClass("move-out");
             });
 
-            $(".carousel__img:nth-child(" + ($(this).index() + 1) + ")").addClass("move-in").one("webkitAnimationEnd", function () {
+            $(".carousel__img:first-child").addClass("move-in").one("webkitAnimationEnd", function () {
                 $(".active").removeClass("active");
                 $(".move-in").addClass("active").removeClass("move-in");
             });
+        } else {
+            $(".active").addClass("move-out").one("webkitAnimationEnd", function () {
+                $(".move-out").removeClass("move-out");
+            });
+
+            imageNext.addClass("move-in").one("webkitAnimationEnd", function () {
+                $(".active").removeClass("active");
+                $(".move-in").addClass("active").removeClass("move-in");
+            });
+        }
+    }, 3000);
+
+    // Xử lí khi click vào nút
+    $(".status__items").click(function () {
+        clearInterval(carousel);
+
+        $(".status__items").removeClass("light");
+
+        $(this).addClass("light");
+
+        $(".active").addClass("move-out").one("webkitAnimationEnd", function () {
+            $(".move-out").removeClass("move-out");
+        });
+
+        $(".carousel__img:nth-child(" + ($(this).index() + 1) + ")").addClass("move-in").one("webkitAnimationEnd", function () {
+            $(".active").removeClass("active");
+            $(".move-in").addClass("active").removeClass("move-in");
         });
     });
 
@@ -71,14 +70,17 @@ $(document).ready(function () {
 
     //Carousel thứ 2:
     $('.slide').slick({
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 3000,
         arrows: false,
         dots: true
     });
-  
+
     //Responsive navbar
     $(window).resize(function () {
         $(".carousel").height($(".carousel__img").height());
     });
 });
+
+
+
