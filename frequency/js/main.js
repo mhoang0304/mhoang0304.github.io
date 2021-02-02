b19b640eb69e63b66b287ce28e4da4f4003d7e3a
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2021-02-02 23:49:32 +0700

    responsive banner

--- frequency/js/main.js
+++ frequency/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
-    console.log("object");
+    // Navigation Mobile
+    $(".nav__moblie-btn").click(function () {
+        $(".nav__moblie").css({"transform": "translateX(0%)", "opacity": "1"});
+        $(".nav__overlay").css("display", "block");
+    });
+
+    $(".nav__overlay").click(function () {
+        $(".nav__moblie").css({"transform": "translateX(-100%)", "opacity": "0"});
+        $(".nav__overlay").css("display", "none");
+    });
+
+    $(".nav__moblie-close--link").click(function () {
+        $(".nav__moblie").css({"transform": "translateX(-100%)", "opacity": "0"});
+        $(".nav__overlay").css("display", "none");
+    });
 });
 
