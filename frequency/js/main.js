f9c814a4c4dbca4f416a6dc885095ee0602d0b6b
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2021-02-14 23:59:43 +0700

    update

--- frequency/js/main.js
+++ frequency/js/main.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
     // Navigation Mobile
     $(".nav__moblie-btn").click(function () {
-        $(".nav__moblie").css({"transform": "translateX(0%)", "opacity": "1"});
+        $(".nav__moblie").css({ "transform": "translateX(0%)", "opacity": "1" });
         $(".nav__overlay").css("display", "block");
     });
 
     $(".nav__overlay").click(function () {
-        $(".nav__moblie").css({"transform": "translateX(-100%)", "opacity": "0"});
+        $(".nav__moblie").css({ "transform": "translateX(-100%)", "opacity": "0" });
         $(".nav__overlay").css("display", "none");
     });
 
     $(".nav__moblie-close--link").click(function () {
-        $(".nav__moblie").css({"transform": "translateX(-100%)", "opacity": "0"});
+        $(".nav__moblie").css({ "transform": "translateX(-100%)", "opacity": "0" });
         $(".nav__overlay").css("display", "none");
+    });
+
+    $("#login").click(function () {
+        $(".login").css("display", "block");
+        $(".nav__overlay-form").css("display", "block");
+    });
+
+    $("#login-close").click(function () {
+        $(".login").css("display", "none");
+        $(".nav__overlay-form").css("display", "none");
     });
 });
 
