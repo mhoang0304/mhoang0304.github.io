342fd56237d14c95ea3b76b22060e8d26ee51f50
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2021-03-01 00:07:09 +0700

    update

---
+++ frequency/js/detail.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // video
+    $("#video--show").click(function () {
+        $(".watch__main-status-video").toggleClass("watch__main-status-video--show");
+    });
+
+    $(".watch__main-status-video--overlay").click(function () {
+        $(".watch__main-status-video").toggleClass("watch__main-status-video--show");
+    });
+
+    // fancybox
+    
+});
