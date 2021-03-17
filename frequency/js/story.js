3cddf1b64ac97eb9d155154c85a56c9c2da3a5f7
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2021-03-17 08:31:44 +0700

    update aboutus.html

--- frequency/js/story.js
+++ frequency/js/story.js
@@ -7,12 +7,31 @@ $(document).ready(function () {
 
     $(window).scroll(function () {
         let scroll = $("html").scrollTop();
+        let section1 = $("#story-1").offset().top;
         let section2 = $("#story-2").offset().top;
         let section3 = $("#story-3").offset().top;
-        console.log(scroll);
+        let section4 = $("#story-4").offset().top;
+        let section5 = $("#story-5").offset().top;
 
-        if (scroll >= (section2 / 2)) {
-            $(".story").removeClass("story-active").next().addClass("story-active");
+        if ((section5 - section2 / 2) <= scroll) {
+            $(".story").removeClass("story-active")
+            $("#story-5").addClass("story-active");
+        } else if ((section4 - section2 / 2) <= scroll) {
+            $(".story").removeClass("story-active")
+            $("#story-4").addClass("story-active");
+            console.log("3");
+        } else if ((section3 - section2 / 2) <= scroll) {
+            $(".story").removeClass("story-active")
+            $("#story-3").addClass("story-active");
+            console.log("3");
+        } else if ((section2 / 2) <= scroll) {
+            $(".story").removeClass("story-active")
+            $("#story-2").addClass("story-active");
+            console.log("2");
+        } else if ((section2 / 2) >= scroll) {
+            console.log("1");
+            $(".story").removeClass("story-active");
+            $("#story-1").addClass("story-active");
         }
     });
 });
