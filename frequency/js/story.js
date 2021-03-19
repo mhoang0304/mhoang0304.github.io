ae84f6f59c6348d51667b91c5db9df5bdf4ad973
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2021-03-19 17:35:16 +0700

    fix animation story.html

--- frequency/js/story.js
+++ frequency/js/story.js
@@ -4,32 +4,29 @@ $(window).on("load", function () {
 });
 
 $(document).ready(function () {
+    $(".nav__main-menu-item:nth-child(3) a").addClass("nav__main--location");
+    $(".nav").css("position", "relative");
 
     $(window).scroll(function () {
         let scroll = $("html").scrollTop();
-        let section1 = $("#story-1").offset().top;
         let section2 = $("#story-2").offset().top;
         let section3 = $("#story-3").offset().top;
         let section4 = $("#story-4").offset().top;
         let section5 = $("#story-5").offset().top;
 
         if ((section5 - section2 / 2) <= scroll) {
-            $(".story").removeClass("story-active")
+            $(".story").removeClass("story-active");
             $("#story-5").addClass("story-active");
         } else if ((section4 - section2 / 2) <= scroll) {
             $(".story").removeClass("story-active")
             $("#story-4").addClass("story-active");
-            console.log("3");
         } else if ((section3 - section2 / 2) <= scroll) {
             $(".story").removeClass("story-active")
             $("#story-3").addClass("story-active");
-            console.log("3");
         } else if ((section2 / 2) <= scroll) {
-            $(".story").removeClass("story-active")
+            $(".story").removeClass("story-active");
             $("#story-2").addClass("story-active");
-            console.log("2");
         } else if ((section2 / 2) >= scroll) {
-            console.log("1");
             $(".story").removeClass("story-active");
             $("#story-1").addClass("story-active");
         }
